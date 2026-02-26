039beb65fd681be1c79a4228665005085c32a842
Himanshu <154112735+himanshusaini1001@users.noreply.github.com>
2026-02-26 20:21:04 +0530

    port managed

--- app.py
+++ app.py
@@ -1222,4 +1222,5 @@ def generate():
 # Run Flask
 # -------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
